742ad5a65b44389d96c894542985e1ad1cdfaf83
Madg26 <manux_852@hotmail.com>
2025-03-11 01:03:04 +0000

    [Proyecto terminado]

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [tarea, setTarea] = useState('');
+  const [tareas, setTareas] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  
+  const handleInputChange = (e) => {
+    setTarea(e.target.value);
+  };
+
+  
+  const handleAddTarea = () => {
+    if (tarea.trim() !== '') {
+      setTareas([...tareas, tarea]);
+      setTarea('');
+    }
+  };
+
+  function deleteTarea  (index) {
+    const eliminar = tareas.filter((_, i) => i !== index);
+    setTareas(eliminar);
+    }
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleAddTarea();
+    }
+  };
+
+  return (
+    <div className="tabla mt-5">
+      <h1>Lista de Tareas</h1>
+
+      <input
+        type="text"
+        value={tarea}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
+        placeholder="Escribe una tarea..."
+      />
+      <ul className="tabla">
+      {tareas.map((tarea, index) => (
+            <li className="taks" key={index}>
+              <span>{tarea}</span>
+              <button
+              className="delete"
+              onClick={() => deleteTarea(index) }
+              >x</button>
+            </li>
+          ))}
+      </ul>
+      <div className="tareasPendientes">
+        <strong>{tareas.length}</strong>
+        <p className="texto">Tareas pendientes</p>
+      </div>
+
+     
+    </div>
+  );
+}
+
 
 export default Home;
