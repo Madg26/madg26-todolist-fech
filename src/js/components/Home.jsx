8d2b1a8f68e5197f8c0405c9a83ec137cdd637e5
Madg26 <manux_852@hotmail.com>
2025-03-19 01:44:29 +0000

    [Proyecto finalizado]

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,33 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 const Home = () => {
+
   const [tarea, setTarea] = useState('');
   const [tareas, setTareas] = useState([]);
 
-  
+  useEffect(() => {
+    getTodos()
+  }, []);
+  function getTodos() {
+    fetch("https://playground.4geeks.com/todo/users/madg26")
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data);
+        setTareas(data.todos)
+      })
+      .catch((err) => { return err })
+  }
+
   const handleInputChange = (e) => {
     setTarea(e.target.value);
+
   };
 
-  
+
   const handleAddTarea = () => {
-    if (tarea.trim() !== '') {
-      setTareas([...tareas, tarea]);
-      setTarea('');
+    if (tarea.trim() === ""){
+      return;
     }
+   
+    fetch("https://playground.4geeks.com/todo/todos/madg26", {
+      method: "POST",
+      body: JSON.stringify({ label: tarea, is_done: "false" }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        setTareas([...tareas, data]);
+        setTarea('');
+      })
+      .catch((err) => { return err })
   };
 
-  function deleteTarea  (index) {
-    const eliminar = tareas.filter((_, i) => i !== index);
-    setTareas(eliminar);
-    }
-
+  function deleteTarea(id) {
+    fetch("https://playground.4geeks.com/todo/todos/" + id, {
+      method: "DELETE"
+    })
+      .then((response) => {
+        return response;
+      }
+      )
+      .then(() => {
+        getTodos()
+      })
+      .catch((err) => { return err });
+  }
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
       handleAddTarea();
+
     }
   };
 
@@ -43,22 +83,22 @@ const Home = () => {
         placeholder="Escribe una tarea..."
       />
       <ul className="tabla">
-      {tareas.map((tarea, index) => (
-            <li className="taks" key={index}>
-              <span>{tarea}</span>
-              <button
+        {tareas.map((tarea, index) => (
+          <li className="taks" key={index}>
+            <span>{tarea.label}</span>
+            <button
               className="delete"
-              onClick={() => deleteTarea(index) }
-              >x</button>
-            </li>
-          ))}
+              onClick={() => deleteTarea(tarea.id)}
+            >x</button>
+          </li>
+        ))}
       </ul>
       <div className="tareasPendientes">
         <strong>{tareas.length}</strong>
         <p className="texto">Tareas pendientes</p>
       </div>
 
-     
+
     </div>
   );
 }
